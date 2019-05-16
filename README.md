df6d4d0f9d1e67e5b375628d252dc1011b55e734
Moutella <joaogabrielmsc@gmail.com>
2019-05-16 15:57:32 -0300

    Criador do arquivo da submissão

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # uri2git
 Scraping URI Online Judge accepted submissions into git repository
+
+dependencies: BeautifulSoup4, GitPython
--- submission.py
+++ submission.py
@@ -1,24 +1,5 @@
-block = True
-line=False
-endings = {"C": "c", 
-            "C#": "cs",
-            "C++": "cpp",
-            "C++17": "cpp", 
-            "C99": "c",     #verificar 
-            "Go": "go",
-            "Haskell": "hs",
-            "Java7": "java",
-            "Java8": "java",
-            "JavaScript": "js",
-            "Kotlin":  "kt",
-            "Lua": "lua",
-            "OCaml": "ml",
-            "Pascal": "pas",
-            "Python 2": "py",
-            "Python 3": "py",
-            "Ruby": "rb",
-            "Scala": "scl",
-            "PostgreSQL": "sql"}
+line=True
+block = False
 comment = {"C": [line, "//"], 
             "C#": [line, "//"], 
             "C==": [line, "//"],
@@ -29,7 +10,7 @@ comment = {"C": [line, "//"],
             "JavaScript": [line, "//"],
             "Kotlin": [line, "//"],
             "Lua": [line, "--"],
-            "OCaml": [block, "(*\ncomment\n*)"],
+            "OCaml": [block, ["(*", "*)"]],
             "Pascal": [line, "//"],
             "Python 2": [line, "#"],
             "Python 3": [line, "#"],
@@ -37,13 +18,30 @@ comment = {"C": [line, "//"],
             "Scala": [line, "//"],
             "PostgreSQL": [line, "--"]}
 
-multiComment = {"C": ["/*", "*/"], 
-                "C#": ["/*", "*/"], 
-                "Python3": ["\"\"\"", "\"\"\""]}
 class Submission:
-    def __init__(self, code, data, tries, language, versao):
-        self.code = code
-        self.tries = tries
+    def __init__(self, id, nome, result, data, language, version, code):
+        self.id = id
+        self.nome = nome
+        self.result = result
         self.data = data
         self.language = language
-        self.versao = versao
+        self.version = version
+        self.code = code
+
+    
+    def createfile(self, submissionFile):
+        commentTemplate = comment[self.language]
+        if(commentTemplate[0]):
+            submissionFile.write(commentTemplate[1] + "Nome: " + self.nome + "\n")
+            submissionFile.write(commentTemplate[1] + "Resultado: " + self.result + "\n")
+            submissionFile.write(commentTemplate[1] + "Data: " + self.data + "\n")
+            submissionFile.write(commentTemplate[1] + "Linguagem: " + self.language + "\n")
+            submissionFile.write(commentTemplate[1] + "Versão: " + str(self.version) + "\n")
+            submissionFile.write(self.code)
+        else:
+            submissionFile.write(commentTemplate[1][0] + "Nome: " + self.nome + "\n")
+            submissionFile.write( "Resultado: " + self.result + "\n")
+            submissionFile.write( "Data: " + self.data + "\n")
+            submissionFile.write( "Linguagem: " + self.language + "\n")
+            submissionFile.write( "Versão: " + str(self.version) + "\n" + commentTemplate[1][1])
+            submissionFile.write(self.code)
