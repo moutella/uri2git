57da232c4d285cb13399a1757295b7192e608520
Moutella <joaogabrielmsc@gmail.com>
2019-05-28 19:01:54 -0300

    parte de login concluída

--- uriscrapper.py
+++ uriscrapper.py
@@ -1,8 +1,14 @@
 from requests import post, get, session
 from bs4 import *
 import os
+import json
 
-
+email = ''
+senha = ''
+with open('auths') as auth:
+    dados = json.load(auth)
+    email = dados['uri'][0]['user-email']
+    senha = dados['uri'][0]['password']
 #### ---------------------LOGIN functions----------------------
 def pegavalores(tags): #Retorna os tokens da sessão para utilização no POST
     valores = {"_csrfToken": "", "_Token[fields]": "", "_Token[unlocked]": ""}
@@ -13,10 +19,6 @@ def pegavalores(tags): #Retorna os tokens da sessão para utilização no POST
     return valores
 
 
-
-
-
-
 ### ---------------- BLOCO DO LOGIN ------------------
 url = "https://www.urionlinejudge.com.br"
 
@@ -25,6 +27,19 @@ siteBase = session.get(url)
 siteSoup = BeautifulSoup(siteBase.text, features="html.parser")
 tagsHidden = siteSoup.find_all("input", type="hidden")
 valores = pegavalores(tagsHidden)
-print(valores)
+payload = {"email": email, "password": senha, "remember_me": "0"}
+payload.update(valores)
+#print(payload)
+urlLogin = "https://www.urionlinejudge.com.br/judge/en/login"
+resultadoLogin = session.post(urlLogin,
+                                data = payload,
+                                headers = dict(referer=urlLogin))
 
-
+## --- BLOCO DE PEGAR TODOS OS ENVIOS ---
+urlSubmissions = "https://www.urionlinejudge.com.br/judge/pt/runs"
+submissionsPage = session.get(urlSubmissions)
+print(submissionsPage.text)
+envioSoup = BeautifulSoup(submissionsPage.text, features="html.parser")
+enviosDaPagina = envioSoup.find_all("tr")
+for envio in enviosDaPagina[1:]:
+    print(envio.a)
